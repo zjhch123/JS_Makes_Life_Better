3ee505c46cfa511dfd8e5b8601c1ab2e9babf1d0
zjhch123 <416836508@qq.com>
2018-07-01 00:02:50 +0800

    study

---
+++ children-practice/src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './App.css';
+import PropTypes from 'prop-types';
+
+class Tabs extends Component {
+
+  static propTypes = {
+    defaultActiveIndex: PropTypes.number
+  }
+
+  constructor() {
+    super()
+    this.state = {
+      index: -1
+    }
+  }
+
+  switchToTab(index) {
+    if (index === this.state.index) { return }
+    this.props.onChange && this.props.onChange(index)
+    this.setState({
+      index
+    })
+  }
+
+  judgeIsActive(order) {
+    const { index } = this.state
+    const { defaultActiveIndex } = this.props
+    return index === -1 ? defaultActiveIndex === order : index === order
+  }
+
+  getTabOrder() {
+    const { children: panels } = this.props
+    return React.Children.map(panels, (child) => {
+      if (!child) return
+      const order = parseInt(child.props.order, 10)
+      const isActive = this.judgeIsActive(order)
+      return (
+        <li 
+          onClick={() => this.switchToTab(order)}
+          style={{color: isActive ? 'red' : 'black'}}>
+          { child.props.tab }
+        </li>
+      )
+    })
+  }
+
+  getTabPanes() {
+    const { children: panels } = this.props
+    return React.Children.map(panels, (child) => {
+      if (!child) return
+      const order = parseInt(child.props.order, 10)
+      const isActive = this.judgeIsActive(order)
+      if (isActive) {
+        return (
+          <div>
+            { child.props.children }
+          </div>
+        )
+      }
+      return;
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          { this.getTabOrder() }
+        </ul>
+        <div>
+          { this.getTabPanes() }
+        </div>
+      </div>
+    )
+  }
+}
+
+class TabPane extends Component {
+  static propTypes = {
+    order: PropTypes.number.isRequired
+  }
+  render() {
+    return (<div>{ this.props.children }</div>)
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Tabs defaultActiveIndex={0} onChange={(order) => console.log(order)}>
+        <TabPane order={0} tab={'Tab1'}>Tab1 Content</TabPane>
+        <TabPane order={1} tab={'Tab2'}>Tab2 Content</TabPane>
+        <TabPane order={2} tab={'Tab3'}>Tab3 Content</TabPane>
+      </Tabs>
+    );
+  }
+}
+
+export default App;
