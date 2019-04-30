6daf2e18834e67ee9593d98823576dd2770462d4
zjhch123 <416836508@qq.com>
2019-05-01 01:10:38 +0800

    study

--- autoGenerator.js
+++ autoGenerator.js
@@ -1,25 +1,45 @@
-function *main() {
-  const ret1 = yield fetch(`https://api.github.com/users/zjhch123`);
-  const j1 = yield ret1.json();
-  console.log(j1)
-}
-
-function co(func) {
-  const gen = func()
-
-  function next(data) {
-    const result = gen.next(data)
-
-    if (result.done) {
-      return
+;(() => {
+  function myFetch(url) {
+    return function(cb) {
+      setTimeout(() => {
+        cb({ code: 200, status: 'success', url })
+      }, 2000)
     }
-
-    result.value.then(data => {
-      next(data)
-    })
   }
-
-  next()
-}
-
-co(main)
+  
+  function *main() {
+    const result = yield myFetch('http://api.github.com/users/zjhch123')
+    const result2 = yield myFetch('http://api.github.com/users/zjhch123')
+    const ret1 = yield fetch(`https://api.github.com/users/zjhch123`)
+    const json1 = yield ret1.json()
+    console.log(result, result2, json1)
+  }
+  
+  function isPromise(func) {
+    return 'then' in func && typeof func.then === 'function'
+  }
+  
+  function co(func) {
+    const gen = func()
+  
+    function next(data) {
+      const result = gen.next(data)
+  
+      if (result.done) {
+        return
+      }
+  
+      if (isPromise(result.value)) {
+        result.value.then(data => {
+          next(data)
+        })
+      } else {
+        result.value(next)
+      }
+    }
+  
+    next()
+  }
+  
+  co(main)
+})()
