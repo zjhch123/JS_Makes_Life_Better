6c304f119e67eca664fa6c9346e559b3945958c4
zjhch123 <416836508@qq.com>
2019-05-01 15:50:53 +0800

    study

--- play-async/index.js
+++ play-async/index.js
@@ -8,14 +8,25 @@ const to = (promise) => {
   })
 }
 
+// ;(async () => {
+//   const fetch1 = to(fetch('https://api.github.com/users/zjhch123'))
+//   const fetch2 = to(fetch('https://api.github.com/users/mqyqingfeng'))
+
+//   const [err1, sec1] = await fetch1
+//   const [err2, sec2] = await fetch2
+
+//   const [j1, j2] = await Promise.all([sec1.json(), sec2.json()])
+
+//   console.log(j1, j2)
+// })()
+
 ;(async () => {
   const fetch1 = to(fetch('https://api.github.com/users/zjhch123'))
   const fetch2 = to(fetch('https://api.github.com/users/mqyqingfeng'))
+  const fetch3 = to(fetch('hhh'))
 
-  const [err1, sec1] = await fetch1
-  const [err2, sec2] = await fetch2
-
-  const [j1, j2] = await Promise.all([sec1.json(), sec2.json()])
-
-  console.log(j1, j2)
+  const r = await Promise.all([fetch1, fetch2, fetch3])
+  
+  console.log(r[2][0])
+  console.log('yeah, return!')
 })()
