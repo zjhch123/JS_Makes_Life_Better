659c21458db087db7a761ab5296115353c553c0f
zjhch123 <416836508@qq.com>
2019-11-10 19:26:25 +0800

    study

--- DP/4.js
+++ DP/4.js
@@ -8,7 +8,6 @@ Example:
 Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
-
 */
 /*
 dp(n) = Max(dp(n-1) + nums[n], nums[n]), 求Max(dp(n)) | n = 0,1,2,3...
